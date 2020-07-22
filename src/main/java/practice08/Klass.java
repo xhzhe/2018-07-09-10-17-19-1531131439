65bf51de91444055ab10df40889c6e0580f1dac7
alex <alex.h.z.xu@oocl.com>
2020-07-22 16:06:41 +0800

    [alex xu] feature: finish klass of practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,35 @@
 package practice08;
 
 public class Klass {
+    private Student leader;
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
+
 }
