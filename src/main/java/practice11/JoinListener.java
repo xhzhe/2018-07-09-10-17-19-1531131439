30a0667241a59516e3c9f7e97b1d6864ba32e7a8
alex <alex.h.z.xu@oocl.com>
2020-07-22 18:21:56 +0800

    [alex xu] feature: Add observer interface, register to listener when teacher is created, finish practice 11

---
+++ src/main/java/practice11/JoinListener.java
@@ -0,0 +1,7 @@
+package practice11;
+
+public interface JoinListener {
+    void studentJoin(Student student);
+
+    void studentBecomeLeader(Student student);
+}
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,73 @@
 package practice11;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Teacher extends Person implements JoinListener {
+    private LinkedList<Klass> classes = new LinkedList<>();
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        this.classes.forEach(klass -> {
+            klass.register(this);
+        });
+    }
+
+
+    public LinkedList<Klass> getKlass() {
+        return classes;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(LinkedList<Klass> classes) {
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    protected String introduce() {
+        if (this.classes == null || this.classes.size() == 0) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            ArrayList<String> classCount = new ArrayList<>();
+            for (Klass klass : classes) {
+                classCount.add(String.valueOf(klass.getNumber()));
+            }
+            return super.introduce() + " I am a Teacher. I teach Class " + String.join(", ", classCount) + ".";
+        }
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass : this.classes) {
+            if (klass.isIn(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected String introduceWith(Student student) {
+        String introduceWith = super.introduce() + " I am a Teacher.";
+        if (isTeaching(student)) {
+            return introduceWith + " I teach " + student.getName() + ".";
+        }
+        return introduceWith + " I don't teach " + student.getName() + ".";
+    }
+
+    @Override
+    public void studentJoin(Student student) {
+        System.out.print(String.format("I am %s. I know %s has joined Class %s.\n", this.getName(), student.getName(), student.getKlass().getNumber()));
+    }
+
+    @Override
+    public void studentBecomeLeader(Student student) {
+        System.out.print(String.format("I am %s. I know %s become Leader of Class %s.\n", this.getName(), student.getName(), student.getKlass().getNumber()));
+    }
+
 }
