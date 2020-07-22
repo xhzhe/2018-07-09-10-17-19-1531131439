b497b9cb8acc4979014abf7cf21fc19441da7002
alex <alex.h.z.xu@oocl.com>
2020-07-22 18:20:03 +0800

    [alex xu] feature: fish klass, person and student in practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,70 @@
 package practice11;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private Student leader;
+    private int number;
+    private final List<JoinListener> teachers;
+
+    public Klass(int number) {
+        this.number = number;
+        this.teachers = new ArrayList<>();
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (!leader.getKlass().equals(this)) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = leader;
+            this.teachers.forEach(teacher -> {
+                teacher.studentBecomeLeader(leader);
+            });
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        this.teachers.forEach(teacher -> {
+            teacher.studentJoin(student);
+        });
+    }
+
+    public boolean isIn(Student student) {
+        return student.getKlass().equals(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    protected void register(JoinListener teacher) {
+        this.teachers.add(teacher);
+    }
 }
--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,38 @@
 package practice11;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    protected String introduce() {
+        String introduce = super.introduce() + " I am a Student.";
+        if (this.klass != null && this.klass.getLeader() != null && this.klass.getLeader().getId() == this.getId()) {
+            introduce += " I am Leader of Class " + this.klass.getNumber() + ".";
+        } else {
+            assert this.klass != null;
+            introduce += " I am at Class " + this.klass.getNumber() + ".";
+        }
+        return introduce;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
 }
