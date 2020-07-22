1a8e571f7844a9a15c0a5f2616fb68e4d911cab5
alex <alex.h.z.xu@oocl.com>
2020-07-22 15:16:19 +0800

    [alex xu][add][finish practice05]

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,24 @@
 package practice05;
 
-public class Student {
+import practice04.Person;
+
+public class Student extends Person {
+    private int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    protected String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + this.klass + ".";
+    }
 }
