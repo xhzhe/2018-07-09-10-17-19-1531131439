a7062aca5530dda6374b9dd9d98926db310b3651
alex <alex.h.z.xu@oocl.com>
2020-07-22 16:18:54 +0800

    [alex xu] feature: finish practice09

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,37 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    protected String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (this.klass == null ? "No Class" : ("Class " + this.klass.getNumber())) + ".";
+    }
+
+    protected String introduceWith(Student student) {
+        String introduceWith = super.introduce() + " I am a Teacher.";
+        if (student.getKlass() == null || this.klass == null) {
+            return introduceWith + " I don't teach " + student.getName() + ".";
+        }
+        if (student.getKlass().getNumber() == this.klass.getNumber()) {
+            return introduceWith + " I teach " + student.getName() + ".";
+        }
+        return introduceWith + " I don't teach " + student.getName() + ".";
+    }
 }
