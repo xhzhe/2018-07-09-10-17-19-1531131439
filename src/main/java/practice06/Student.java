a2584332c671031f4022604aad5d60ea8f84807f
alex <alex.h.z.xu@oocl.com>
2020-07-22 15:28:16 +0800

    [alex xu] feature: finish practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Student {
+import practice04.Person;
+
+public class Student extends Person {
+    private Integer klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    protected String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + this.klass + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    protected String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (this.klass == null ? "No Class" : ("Class " + this.klass)) + ".";
+    }
 }
