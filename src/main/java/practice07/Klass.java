034c28146f8a6ad6fa088db1d24c184fa7fdbfb1
alex <alex.h.z.xu@oocl.com>
2020-07-22 15:43:20 +0800

    [alex xu] feature: finish Klass's coding

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,21 @@
 package practice07;
 
 public class Klass {
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    private int number;
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
 }
