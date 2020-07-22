2f6130daae39811b123934e2c9e1a4ea1f1cf931
alex <alex.h.z.xu@oocl.com>
2020-07-22 16:23:57 +0800

    [alex xu] feature: finish klass, person and student of practice10

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,50 @@
 package practice10;
 
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    protected String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
 }
--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,33 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    protected String introduce() {
+        String introduce = super.introduce() + " I am a Student.";
+        if (this.klass != null && this.klass.getLeader() != null && this.klass.getLeader().getId() == this.getId()) {
+            introduce += " I am Leader of Class " + this.klass.getNumber() + ".";
+        } else {
+            assert this.klass != null;
+            introduce += " I am at Class " + this.klass.getNumber() + ".";
+        }
+        return introduce;
+    }
 }
