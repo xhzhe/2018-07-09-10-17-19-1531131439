4f00a67ea83cce0c082c3f5be09f3f65294cf969
alex <alex.h.z.xu@oocl.com>
2020-07-22 16:18:37 +0800

    [alex xu] feature: finish klass of practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,45 @@
 package practice09;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private Student leader;
+    private int number;
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (students.isEmpty()) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = leader;
+        }
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
